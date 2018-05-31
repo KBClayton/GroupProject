b5710b273ba97917d43499af1f8b5da665973def
KBClayton <clayton.kevinb@gmail.com>
2018-05-30 20:18:32 -0500

    Markers and cleaned loops

--- assets/javascript/maps.js
+++ assets/javascript/maps.js
@@ -1,33 +1,43 @@
 
     //Start Clayton's variables
-    // observer_location needs to be the observer information from the input
-    var observer_location = {lat: 30, lng: -97};
+    var observer_location = {lat: 30, lng: -97}; // observer_location needs to be the observer information from the input
     //tlestring needs to be the tle response from the n2yo tle search api (response.tle)
     //var tlestring="1 25544U 98067A   18138.88883277 +.00001406 +00000-0 +28541-4 0  9997\r\n2 25544 051.6394 164.7606 0004002 099.1102 320.7609 15.54070224113965";
-    //tle element used to describe the average movement per day, geostationary is 1.0027
-    var mean_motion=1.01;
-    //startUTC and endUTC are from pass data from n2yo pass (visual or radio) search api (response.passes[0].startUTC and response.passes[0].endUTC)
-    var startUTC=1526901635;
-    var endUTC=1526902240;
-    //other time variables
-    var setback_time;
-    var setbackutc;
-    var roll_endUTC;
-    var interval_time;
+    var mean_motion=1.01;     //tle element used to describe the average movement per day, geostationary is 1.0027
+    var satrec; //will hold sattelite object
+    //time variables
+    var startUTC=1526901635; //start of a pass
+    var endUTC=1526902240; //end of a pass
+    var firstUTC; //first of the first pass
+    var finalUTC; //last of the last pass
+    var setback_time; //time before first pass to draw orbit
+    var setbackutc; //utc time before first pass
+    var roll_endUTC;  //end of the calculated path time
+    var interval_time; //how long between orbit calculations when drawing out the path
     //map variables so they can be reached globally
-    var marker_roll_start;
-    var marker_roll_start_pos;
-    var marker_roll_end;
-    var marker_roll_end_pos;
-    var observer_marker;
-    var marker_enter;
-    var marker_exit;
-    var satPath;
-    var marker_array=[];
-    //elevation & elevator to get return from google elevation query
-    var elevation=0;
-    var elevator;
+    var marker_roll_start;  //marker at beggining of orbit path calculations
+    var marker_roll_start_pos; //position at beggining of orbit path calculations
+    var marker_roll_end; //marker at end of orbit path calculation
+    var marker_roll_end_pos; //position at end of orbit path calculations
+    var observer_marker; //where the observer is from the search
+    var marker_enter; // marker at start of a pass
+    var marker_exit; //marker at end of pass
+    var satPath; //overall path
+    var marker_array=[]; // contains all set markers for clearing
+    var elevation=0;  //elevation above sea level in meters set by api
+    var elevator; //set to google maps function
     //End Clayton's variables
+
+    //wait function
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
+      }
+      
+      async function wait() {
+        await sleep(2000);
+    }
+    //end wait function
+
 
     //Start Clayton's Object
     var mapobject = {
@@ -36,6 +46,7 @@
             observer_location.lng= parseFloat(longSearch);
             observer_marker = new google.maps.Marker({
                 position: observer_location,
+                icon: 'assets/images/markers/red_MarkerO.png',
                 title:"Observation Point",
             });
             map.setCenter(new google.maps.LatLng( latSearch, longSearch));
@@ -43,6 +54,7 @@
             observer_marker.setMap(map);
             marker_array.push(observer_marker);
         },
+
         elevation: function(){
             elevator = new google.maps.ElevationService;
             elevator.getElevationForLocations({
@@ -59,45 +71,41 @@
                 }
               });
         },
+
         draw: function(){
-            //tlestring split into 2 parts for entry into sattelite 
-            var tle1=tlestring.split("\r\n")[0];
-            var tle2=tlestring.split("\r\n")[1];
+            //tlestring split into 2 parts for entry into sattelite.min.js
+            var tle1=tlestring.split("\r\n")[0]; //line1
+            var tle2=tlestring.split("\r\n")[1]; //line2
             mean_motion=parseFloat(tle2.substring(52, 62));
-            if (mean_motion<1.0031 && 1.0021<mean_motion){
-                console.log("Probably geostationary or geosynchronous, map will be point or small line");
-            }
-            //sattelite object ititalized with tle elements
-            var satrec = satellite.twoline2satrec(tle1, tle2);
+            firstUTC=passArray[0].startUTC;
+            finalUTC=passArray[(passArray.length-1)].endUTC;
+            satrec = satellite.twoline2satrec(tle1, tle2);  //sattelite object ititalized with tle elements
             //loop to build path between entry and exit points
             for(var i=0; i<passArray.length; i++ ){
+                //set time element for pass
                 startUTC=passArray[i].startUTC;
                 endUTC=passArray[i].endUTC;
-                //entry point code
+                //entry point code for each pass
                 var starttime=moment.utc(startUTC,'X').toDate();
-                console.log(starttime);
                 var positionAndVelocity = satellite.propagate(satrec, starttime);
                 var positionEci = positionAndVelocity.position;
                 var velocityEci = positionAndVelocity.velocity;
-                console.log("starttime: "+starttime);
                 var gmst = satellite.gstime(starttime);
-                console.log("gmst: "+gmst);
-                console.log("positionEci: "+positionEci);
                 var positionGd = satellite.eciToGeodetic(positionEci, gmst);
                 var longitude = positionGd.longitude;
                 var latitude  = positionGd.latitude;
                 var longitudeStr = satellite.degreesLong(longitude);
                 var latitudeStr  = satellite.degreesLat(latitude);
-                //google map code for entry point merker
+                //google map code for entry point marker
                 var enter={lat: latitudeStr, lng: longitudeStr };
                 marker_enter = new google.maps.Marker({
                     position: enter,
-                    title: "Enters visibility on pass "+(i+1)+" on "+moment.utc(startUTC, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(startUTC, 'X').local().format('h:mm:ss a')
+                    icon: 'assets/images/markers/purple_MarkerS.png',
+                    title: "Start of visibility on pass "+(i+1)+" on "+moment.utc(startUTC, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(startUTC, 'X').local().format('h:mm:ss a')
                 });
                 marker_array.push(marker_enter);
                 marker_enter.setMap(map); 
-
-                //exit point code
+                //exit point code for each pass
                 var endtime=moment.utc(endUTC,'X').toDate();
                 var exit_positionAndVelocity = satellite.propagate(satrec, endtime);
                 var exit_positionEci = exit_positionAndVelocity.position;
@@ -112,19 +120,19 @@
                 //google map code for exit point marker
                 marker_exit = new google.maps.Marker({
                     position: exit,
-                    title: "Leaves visibility on pass "+(i+1)+" on "+moment.utc(endUTC, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(endUTC, 'X').local().format('h:mm:ss a')
+                    icon: 'assets/images/markers/yellow_MarkerE.png',
+                    title: "End of visibility on pass "+(i+1)+" on "+moment.utc(endUTC, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(endUTC, 'X').local().format('h:mm:ss a')
                 });
-                marker_array.push(marker_exit);
                 marker_exit.setMap(map);
-
+                marker_array.push(marker_exit);
                 //draw line between entry and exit
                 var satPathCoordinates = [];
                 satPathCoordinates.push({lat: latitudeStr, lng: longitudeStr});
                 //intervening points on pass
-                var visual_increment=(endUTC-startUTC)/100;
+                var visual_increment=(endUTC-startUTC)/200;
                 var visual_time=0;
-                for(var i=1; i<100; i++){
-                    var vistime=moment.utc((startUTC+(i*visual_increment)),'X').toDate();
+                for(var r=1; r<200; r++){
+                    var vistime=moment.utc((startUTC+(r*visual_increment)),'X').toDate();
                     var vis_positionAndVelocity = satellite.propagate(satrec, vistime);
                     var vis_positionEci = vis_positionAndVelocity.position;
                     var vis_velocityEci = vis_positionAndVelocity.velocity;
@@ -144,39 +152,24 @@
                     strokeOpacity: 1.0,
                     strokeWeight: 2
                 });
-                satPath.setMap(map);
                 marker_array.push(satPath);
+                satPath.setMap(map);
             }   
-            //set time to draw orbit for
+            //set time to draw orbit for less for faster objects (mean_motion difference from 1.0027)
             setback_time=100.1;
             interval_time=5.1;
-            if(10<=mean_motion){
-                setback_time=3600;
-                interval_time=7.2;
+            setback_time=(finalUTC-firstUTC)/100;
+            setback_time+=3600;
+            interval_time=((finalUTC-firstUTC)+setback_time)/1200;
+            var loop_iterations=1000;
+            if ((loop_iterations*interval_time) <= ((finalUTC-firstUTC) + setback_time)){
+                while((loop_iterations*interval_time) <= ((finalUTC-firstUTC) + setback_time)){
+                    loop_iterations+=10;   
+                }
             }
-            if(4<mean_motion && mean_motion<10){
-                setback_time=36000;
-                interval_time=72;
-            }
-            if(2<mean_motion && mean_motion<=4){
-                setback_time=180000;
-                interval_time=360;
-            }
-            if(1.4<=mean_motion && mean_motion<=2){
-                setback_time=360000;
-                interval_time=720;
-            }
-            if(1.1<=mean_motion && mean_motion<1.4){
-                setback_time=9000000;
-                interval_time=1800;
-            }
-            if(mean_motion<1.1){
-                setback_time=360000;
-                interval_time=720;
-            }
-            //set up for orbital path data
+            loop_iterations+=10;
             var roll_LineCoord=[];
-            for(var i=0; i<1000; i++){
+            for(var i=0; i<loop_iterations; i++){
                 setbackutc=passArray[0].startUTC-setback_time;
                 var rolltime=moment.utc((setbackutc+(i*interval_time)),'X').toDate();
                 var roll_positionAndVelocity = satellite.propagate(satrec, rolltime);
@@ -192,26 +185,27 @@
                 if(i===0){
                     marker_roll_start_pos={lat: roll_latitudeStr, lng: roll_longitudeStr};
                 }
-                if(i===999){
+                if(i===(loop_iterations-1)){
                     marker_roll_end_pos={lat: roll_latitudeStr, lng: roll_longitudeStr};
                     roll_endUTC=setbackutc+(i*interval_time);
                 }
             }
-            //markers on begin and end  of roll pathpath
+            //markers on begin and end  of roll path
             marker_roll_start = new google.maps.Marker({
                 position: marker_roll_start_pos,
-                title: "Start of orbital path calculation on "+moment.utc(setbackutc, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(setbackutc, 'X').local().format('h:mm:ss a')
+                icon: 'assets/images/markers/orange_MarkerB.png',
+                title: "Begining of orbital path calculation on "+moment.utc(setbackutc, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(setbackutc, 'X').local().format('h:mm:ss a')
             });
             marker_array.push(marker_roll_start);
             marker_roll_start.setMap(map);
-
             marker_roll_end = new google.maps.Marker({
                 position: marker_roll_end_pos,
-                title: "End of orbital path calculation on "+moment.utc(roll_endUTC, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(roll_endUTC, 'X').local().format('h:mm:ss a')
+                icon: 'assets/images/markers/brown_MarkerF.png',
+                title: "Final orbital path calculation on "+moment.utc(roll_endUTC, 'X').local().format('dddd MMMM Do YYYY')+" at "+moment.utc(roll_endUTC, 'X').local().format('h:mm:ss a')
             });
             marker_array.push(marker_roll_end);
             marker_roll_end.setMap(map);
-            //drawing 
+            //drawing  the roll path
             roll_satPath = new google.maps.Polyline({
                 path: roll_LineCoord,
                 geodesic: true,
@@ -221,16 +215,14 @@
             });
             marker_array.push(roll_satPath);
             roll_satPath.setMap(map);
-
-
-
         },
+
         map_clear: function (){
             if(marker_array.length>0){
                 for(var i=0; i<marker_array.length; i++){
-                    marker_array[i].setMap(null)
+                    marker_array[i].setMap(null); //removes markers from map
                 }
-                marker_array=[];
+                marker_array=[]; //clears array
             }
         }        
     }
@@ -242,7 +234,9 @@
         $("#map").css("display", "inline");
         mapobject.initial_map();
         mapobject.elevation();
-        mapobject.draw();
+        wait().then(function(){
+            mapobject.draw();
+        });
     });
     //End Clayton's click handler
 
